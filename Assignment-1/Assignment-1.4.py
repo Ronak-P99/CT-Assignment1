83bea594e08c2c8d3c56e117bb40921e82c85564
Ronak-P99 <163923640+Ronak-P99@users.noreply.github.com>
2024-03-20 17:49:02 -0500

    Assignment-1.4.py

--- Assignment-1/Assignment-1.4.py
+++ Assignment-1/Assignment-1.4.py
@@ -1,4 +1,5 @@
 #Task 4.1
+
 price_bread = 2.50
 price_pb = 3.27
 price_jelly = 5.62
@@ -7,6 +8,7 @@ total_groceries = price_bread + price_pb + price_jelly
 print(total_groceries)
 
 #Task 4.2
+
 initial_amount = 10000
 fixed_rate = .07
 
